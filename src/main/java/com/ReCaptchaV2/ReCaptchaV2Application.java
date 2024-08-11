dffe11506606a09c021536492a156b31d173f885
damianrein <damianrein464@gmail.com>
2024-08-10 21:48:58 -0300

    -

---
+++ src/main/java/com/ReCaptchaV2/ReCaptchaV2Application.java
@@ -0,0 +1,13 @@
+package com.ReCaptchaV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReCaptchaV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReCaptchaV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ReCaptchaV2/ReCaptchaV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ReCaptchaV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReCaptchaV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
