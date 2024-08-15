68b405432e87377bc223cac3f46103e834814243
damianrein <damianrein464@gmail.com>
2024-08-15 19:01:17 -0300

    -

--- src/main/java/com/ReCaptchaV2/services/ReCaptchaService.java
+++ src/main/java/com/ReCaptchaV2/services/ReCaptchaService.java
@@ -1,8 +1,19 @@
 package com.ReCaptchaV2.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class ReCaptchaService {
 
+    private static final String GOOGLE_RECAPTCHA_ENDPOINT = "https://www.google.com/recaptcha/api/siteverify";
+
+    private final String RECAPTCHA_SECRET = "6LfKJeAmAAAAAAxaZgRToYjrvj8b3Z9r09jdx6U2";
+	
+	public boolean validateCaptcha(String captcha) {
+		RestTemplate restTemplate = new RestTemplate();
+		
+		
+		return true;
+	}
 }
